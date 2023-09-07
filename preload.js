b2ac4609b8b53f8da2c2386b7f7cc5c42509ed18
hichamelb <hichamelb54@gmail.com>
2023-09-07 22:18:29 +0100

    about page

--- preload.js
+++ preload.js
@@ -1,8 +1,9 @@
-const { contextBridge, clipboard } = require("electron");
+const { contextBridge, clipboard, shell } = require("electron");
 
 contextBridge.exposeInMainWorld("context", {
     readClipboard: () => clipboard.readText(),
     writeClipboard: (text) => clipboard.writeText(text),
     clearClipboard: () => clipboard.clear(),
+    openUrl: (url) => shell.openExternal(url),
     version: process.env.npm_package_version,
 });
