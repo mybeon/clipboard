fd60bb5e014826acb459516b0f2bd0cca52bd6fe
hichamelb <hichamelb54@gmail.com>
2023-09-26 01:35:53 +0100

    refactor code to ListElement class

---
+++ renderer/models/User.js
@@ -0,0 +1,39 @@
+import truncate from "../utils/truncate.js";
+import formatDate from "../utils/formatDate.js";
+import { list } from "../script.js";
+const notification = document.querySelector(".notification");
+
+export class ListElement {
+    constructor(text) {
+        this.text = text;
+        this.id = Math.ceil(Math.random() * 100 * 100);
+        this.init();
+        this.insertedEl = document.getElementById(this.id);
+        this.listener();
+    }
+
+    init() {
+        const element = `
+        <li data-content="${this.text}" id="${this.id}">
+            <img src="./assets/icons/doc.svg" />
+            <div class="content">
+                <p>${truncate(this.text)}</p>
+                <span>${formatDate(Date.now())}</span>
+            </div>
+        </li>
+        `;
+        list.insertAdjacentHTML("afterbegin", element);
+    }
+
+    listener() {
+        this.insertedEl.addEventListener("click", this.onClick.bind(this));
+    }
+
+    onClick(e) {
+        context.writeClipboard(this.text);
+        notification.classList.add("active");
+        setTimeout(() => {
+            notification.classList.remove("active");
+        }, 1800);
+    }
+}
--- renderer/script.js
+++ renderer/script.js
@@ -1,11 +1,9 @@
-import truncate from "./utils/truncate.js";
-import formatDate from "./utils/formatDate.js";
-const list = document.querySelector("#list");
-const notification = document.querySelector(".notification");
+import { ListElement } from "./models/User.js";
 const clearBtn = document.querySelector("#clear");
 
-let prevText;
-let content = [];
+export const list = document.querySelector("#list");
+export var prevText;
+export let content = [];
 
 clearBtn.addEventListener("click", () => {
     list.innerHTML = null;
@@ -14,47 +12,11 @@ clearBtn.addEventListener("click", () => {
     context.clearClipboard();
 });
 
-class ListElement {
-    constructor(text) {
-        this.text = text;
-        this.id = Math.ceil(Math.random() * 100 * 100);
-        this.init();
-        this.insertedEl = document.getElementById(this.id);
-        this.listener();
-    }
-
-    init() {
-        const element = `
-        <li data-content="${this.text}" id="${this.id}">
-            <img src="./assets/icons/doc.svg" />
-            <div class="content">
-                <p>${truncate(this.text)}</p>
-                <span>${formatDate(Date.now())}</span>
-            </div>
-        </li>
-        `;
-        list.insertAdjacentHTML("afterbegin", element);
-        content.push(this.text);
-        prevText = this.text;
-    }
-
-    listener() {
-        this.insertedEl.addEventListener("click", this.onClick.bind(this));
-    }
-
-    onClick(e) {
-        context.writeClipboard(this.text);
-        notification.classList.add("active");
-        setTimeout(() => {
-            notification.classList.remove("active");
-        }, 1800);
-    }
-}
-
 setInterval(() => {
     const text = context.readClipboard();
     if (text.trim() !== "" && prevText !== text && !content.includes(text)) {
         new ListElement(text);
-        return;
+        content.push(text);
+        prevText = text;
     }
 }, 1000);
