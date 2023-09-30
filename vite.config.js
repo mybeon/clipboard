02a89f3d308197231bed1cca6d8bbd657b344056
hichamelb <hichamelb54@gmail.com>
2023-09-30 18:27:47 +0100

    fix UI rendering twice in dev mode
    the UI wasn't rendering correctly in dev mode because the app was loading js files twice from two different paths ("./", "/")

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,24 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-    root: "./app/src/",
-    base: "",
-    server: {
-        strictPort: true,
-        port: 3000,
-    },
-    build: {
-        emptyOutDir: true,
-        minify: "terser",
-        outDir: "../build/",
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "app/src/home.html"),
-                about: resolve(__dirname, "app/src/about.html"),
+export default defineConfig(({ command }) => {
+    return {
+        root: "./app/src/",
+        base: command === "serve" ? "/" : "",
+        server: {
+            strictPort: true,
+            port: 3000,
+        },
+        build: {
+            emptyOutDir: true,
+            minify: "terser",
+            outDir: "../build/",
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, "app/src/home.html"),
+                    about: resolve(__dirname, "app/src/about.html"),
+                },
             },
         },
-    },
+    };
 });
