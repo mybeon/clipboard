77ea0c0e663e3860e4085ae2c7ad140390a3585d
hichamelb <hichamelb54@gmail.com>
2023-11-07 21:13:57 +0100

    resolve #13: set up react vitejs

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ command }) => {
@@ -9,16 +9,17 @@ export default defineConfig(({ command }) => {
             strictPort: true,
             port: 3000,
         },
+        plugins: [react()],
         build: {
             emptyOutDir: true,
             minify: "terser",
             outDir: "../build/",
-            rollupOptions: {
-                input: {
-                    main: resolve(__dirname, "app/renderer/src/home.html"),
-                    about: resolve(__dirname, "app/renderer/src/about.html"),
-                },
-            },
+            // rollupOptions: {
+            //     input: {
+            //         main: resolve(__dirname, "app/renderer/src/home.html"),
+            //         about: resolve(__dirname, "app/renderer/src/about.html"),
+            //     },
+            // },
         },
     };
 });
